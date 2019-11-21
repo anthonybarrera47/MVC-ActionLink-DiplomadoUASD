7907e4fceaccc1194eadc4b79cdfcb99970d4fab
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2019-11-21 19:11:43 -0400

    Tarea 1

---
+++ Models/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_ActionLink_DiplomadoUASD.Models
+{
+    public class Clientes
+    {
+
+    }
+}
---
+++ Models/Proveedores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_ActionLink_DiplomadoUASD.Models
+{
+    public class Proveedores
+    {
+        public int ProveedorId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
